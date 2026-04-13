494a8f8e69c515cd3914db3a30458b32a93a43df
AdriaanGeldenhuis <35817022+AdriaanGeldenhuis@users.noreply.github.com>
2026-04-13 15:46:27 +0200

    Add files via upload

---
+++ app files/app/src/main/java/app/flowwork/MainActivity.java
@@ -0,0 +1,46 @@
+package app.flowwork;
+
+import android.os.Bundle;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Create WebView programmatically
+        webView = new WebView(this);
+        setContentView(webView);
+
+        // WebView settings
+        WebSettings settings = webView.getSettings();
+        settings.setJavaScriptEnabled(true);
+        settings.setDomStorageEnabled(true);
+        settings.setDatabaseEnabled(true);
+        settings.setCacheMode(WebSettings.LOAD_DEFAULT);
+        settings.setMixedContentMode(WebSettings.MIXED_CONTENT_ALWAYS_ALLOW);
+
+        // WebViewClient
+        webView.setWebViewClient(new WebViewClient());
+        webView.setWebChromeClient(new WebChromeClient());
+
+        // Load URL
+        webView.loadUrl("https://www.flowwork.app");
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
